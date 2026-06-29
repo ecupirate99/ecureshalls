992fc80663006e10e759bb87418f543176525a4b
ecupirate99 <ecupirate99@gmail.com>
2026-06-29 14:28:10 -0400

    Update query.js

--- api/query.js
+++ api/query.js
@@ -1,7 +1,9 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const body = req.body;
+      const body = typeof req.body === "string"
+        ? JSON.parse(req.body)
+        : req.body;
 
       res.status(200).json({
         success: true,
