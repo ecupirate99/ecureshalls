cdca2c51ac62192ac6a777acb7034b3dfccab471
ecupirate99 <ecupirate99@gmail.com>
2026-06-29 13:04:46 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.sendStatus(204);
+});
 app.get('/test', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.json({ status: 'ok' });
 });
 
-app.post('/api/query', async (req, res) => {
-  const data = await queryHandler(req, res);
-  return res.status(200).json(data);
-});
+app.post('/api/query', queryHandler);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
